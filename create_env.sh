0d0823625becb2b2b8ec39ffdd8710b2c0249d07
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-09-22 10:49:24 -0300

    Create folders at root

--- create_env.sh
+++ create_env.sh
@@ -5,17 +5,18 @@ declare -a usuarios=("carlos" "debora" "josefina" "maria" "sebastiana" "amanda" 
 declare -a grupos=("GRP_ADM" "GRP_VEN" "GRP_SEC")
 declare -a diretorios=("publico" "adm" "ven" "sec")
 
-# Criar usuários
+# Criar usuários com bash e senha padrão
 for i in "${usuarios[@]}"
 do
 	echo "Criando usuario $i ..."
 	useradd -m -c "$i" -s /bin/bash "$i" -p $(openssl passwd -crypt Senha123)
 done
 
-# for i in "${diretorios[@]}"
-# do
-# 	echo "Criando diretorio $i ..."
-# 	mkdir /"$i"
-# done
+# Criar diretórios na raiz do sistema
+for i in "${diretorios[@]}"
+do
+	echo "Criando diretorio $i ..."
+	mkdir /"$i"
+done
 
 
